4135b051b30a42dddc9c9d930426758efa9ff1e7
unknown <s.dg.emir@gmail.com>
2021-08-04 17:05:08 +0200

    initializing train_cnn.py

---
+++ train_cnn.py
@@ -0,0 +1,20 @@
+'''
+This module should be the main module for training and validating data.
+This module should also send data to TensorBoard.
+'''
+import torch 
+from torch.utils.tensorboard import SummaryWriter
+from utils import visualizer
+from architectures import resnet_architecture
+
+
+def check_validation_set(model):
+    pass 
+
+
+def train_nn():
+    pass 
+
+
+if __name__ == '__main__':
+    pass 
