64c7f9744789d35d634f0970cdac3178cd1c30f9
unknown <s.dg.emir@gmail.com>
2021-08-04 18:34:04 +0200

    small updates

--- train_cnn.py
+++ train_cnn.py
@@ -6,17 +6,22 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 from utils import visualizer
 from architectures import pretrained_architectures
+from colorama import Fore
 
 
-def loss_observer(loss, step, epoch):
+def loss_observer(loss : float, step : int, epoch : int) -> str:
     observed = f'Step: {step} ||| Epoch: {epoch} ||| LOSS: {loss}'
 
     return observed
 
 
-def loss_warning(loss_history, threshold):
+def loss_warning(loss_history : list, threshold : int) -> str:
+    '''
+    Method checks whether the last n losses are constantly increasing.
+    If so, returns a string with the specified warning. Otherwise, method returns None.
+    '''
     if sorted(loss_history[-threshold: ]) == loss_history[-threshold: ]:
-        warning = '\n||| LOSS HAS BEEN CONSTANTLY INCREASING FOR THE LAST {threshold} STEPS |||'
+        warning = Fore.RED + '\n||| LOSS HAS BEEN CONSTANTLY INCREASING FOR THE LAST {threshold} STEPS |||\n'
         return warning 
     else:
         return None
@@ -39,9 +44,15 @@ def train_nn(model, optimizer, criterion, learning_rate, dataloaders, epochs, de
         epochs
         device
         scheduler 
-        
+
     '''
-    
+    summary_writer = SummaryWriter()
+    step = 0 
+    train_epoch_loss = 0
+    train_step_loss = 0
+    val_epoch_loss = 0
+    val_step_loss = 0
+
     for _ in range(epochs):
 
         for phase in ['train', 'evaluate']:
@@ -49,7 +60,7 @@ def train_nn(model, optimizer, criterion, learning_rate, dataloaders, epochs, de
             if phase == 'train':
                 model.train()
             elif phase == 'evaluate':
-                model.evalu()
+                model.eval()
             
             for features, labels in dataloaders[phase]:
                 features = features.to(device)
@@ -65,8 +76,15 @@ def train_nn(model, optimizer, criterion, learning_rate, dataloaders, epochs, de
                 if scheduler == True:
                     learning_rate.step()
 
-    pass 
+                step += 1
+                #step_loss += loss 
+                #summary_writer.add_scalars('Train Loss', step_loss, step)
+
+        train_epoch_loss  = train_step_loss - train_epoch_loss
+        val_epoch_loss = val_step_loss - val_epoch_loss
+
+    return None
 
 
 if __name__ == '__main__':
-    print(None)
+    pass 
