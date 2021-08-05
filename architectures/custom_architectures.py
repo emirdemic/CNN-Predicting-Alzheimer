8745d1abc45fc88f5613d3339e3a70216ecef5e3
unknown <s.dg.emir@gmail.com>
2021-08-05 17:20:55 +0200

    implemented forward propagation for logistic regression

--- architectures/custom_architectures.py
+++ architectures/custom_architectures.py
@@ -8,3 +8,13 @@ class MyModel(torch.nn.Module):
 
     def forward(self, x):
         pass 
+
+
+class LogisticRegression(torch.nn.Module):
+    def __init__(self, input_dims, output_dims):
+        super(LogisticRegression, self).__init__()
+        self.layer = torch.nn.Linear(input_dims, output_dims)
+
+    def forward(self, x):
+        linear_combination = self.layer(x)
+        return linear_combination
