279cac46b384b508f796d994eb47eda5a6dfb7f1
unknown <s.dg.emir@gmail.com>
2021-08-05 19:12:02 +0200

    updated hyperparameters

--- architectures/network_hyperparameters.py
+++ architectures/network_hyperparameters.py
@@ -1,11 +1,13 @@
 import torch 
 
-resnet50 = {
+resnet = {
     'OPTIMIZER' : torch.optim.Adam, 
-    'LEARNING_RATE' : torch.optim.lr_scheduler.ReduceLROnPlateau,
+    'LEARNING_RATE' : 0.0001,
     'CRITERION' : torch.nn.CrossEntropyLoss, 
     'EPOCHS' : 1000, 
-}
+    'SCHEDULER' : False,
+    'DEVICE' : 'cuda' if torch.cuda.is_available() else 'cpu'
+    }
 
 vgg19 = {
     'OPTIMIZER' : torch.optim.Adam,
