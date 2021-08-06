eda8f2067648eaf2d4cc567281f4b12071b3e49d
unknown <s.dg.emir@gmail.com>
2021-08-06 16:16:10 +0200

    added one argument to AddGaussianNoise

--- utils/transformations.py
+++ utils/transformations.py
@@ -1,14 +1,14 @@
 import torch 
 
 class AddGaussianNoise(torch.nn.Module):
-    def __init__(self, mean = 0, std = 1):
+    def __init__(self, mean = 0, std = 1, device = 'cuda'):
         super(AddGaussianNoise, self).__init__()
         self.mean = 0
         self.std = 1 
-    
+        self.device = device
 
     def __call__(self, tensor):
-        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+        return tensor + torch.randn(tensor.size()).to(device = self.device) * self.std + self.mean
 
 
     def __repl__(self):
