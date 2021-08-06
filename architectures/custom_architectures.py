97c2cb045471a5d594579d2f7e055e1399452233
unknown <s.dg.emir@gmail.com>
2021-08-06 16:15:14 +0200

    debugged siamese cnn

--- architectures/custom_architectures.py
+++ architectures/custom_architectures.py
@@ -4,76 +4,76 @@ from utils.transformations import AddGaussianNoise
 
 
 class SiameseCNN(torch.nn.Module):
-    def __init__(self, input_dims, output_dims):
+    def __init__(self, input_dims, output_dims, device):
         super(SiameseCNN, self).__init__()
 
         self.input_dims = input_dims 
         self.output_dims = output_dims
+        self.device = device
 
-        self.layers = OrderedDict()
-        self.layers['layer1'] = torch.nn.Sequential(
+        self.layer1 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = input_dims, out_channels = 64, kernel_size = 3),
             torch.nn.ReLU(),
             torch.nn.BatchNorm2d(num_features = 64)
         )
-        self.layers['layer2'] = torch.nn.Sequential(
+        self.layer2 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size = 2)
         )
-        self.layers['layer3'] = torch.nn.Sequential(
+        self.layer3 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3),
             torch.nn.ReLU(),
             torch.nn.BatchNorm2d(num_features = 128),
-            AddGaussianNoise()
+            AddGaussianNoise(device = self.device)
         )
-        self.layers['layer4'] = torch.nn.Sequential(
+        self.layer4 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size = 2)
         )
-        self.layers['layer5'] = torch.nn.Sequential(
+        self.layer5 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = 3),
             torch.nn.ReLU(),
             torch.nn.BatchNorm2d(num_features = 256),
         )
-        self.layers['layer6'] = torch.nn.Sequential(
+        self.layer6 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3),
             torch.nn.ReLU()
         )
-        self.layers['layer7'] = torch.nn.Sequential(
+        self.layer7 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3),
             torch.nn.ReLU(),
-            AddGaussianNoise()
+            AddGaussianNoise(device = self.device)
         )
-        self.layers['layer8'] = torch.nn.Sequential(
+        self.layer8 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size = 2)
         )
-        self.layers['layer9'] = torch.nn.Sequential(
+        self.layer9 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 3),
             torch.nn.ReLU()
         )
-        self.layers['layer10'] = torch.nn.Sequential(
+        self.layer10 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 512, out_channels = 512, kernel_size = 3),
             torch.nn.ReLU()
         )
-        self.layers['layer11'] = torch.nn.Sequential(
+        self.layer11 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 512, out_channels = 512, kernel_size = 3),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size = 2)
         )
-        self.layers['layer12'] = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels = 512, out_channels = 256, kernel_size = 3),
+        self.layer12 = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels = 512, out_channels = 512, kernel_size = 3),
             torch.nn.ReLU(),
-            AddGaussianNoise()
+            AddGaussianNoise(device = self.device)
         )
-        self.layers['layer13'] = torch.nn.Sequential(
+        self.layer13 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 512, out_channels = 512, kernel_size = 3),
             torch.nn.ReLU()
         )
-        self.layers['layer14'] = torch.nn.Sequential(
+        self.layer14 = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels = 512, out_channels = 256, kernel_size = 3),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size = 2)
@@ -81,17 +81,28 @@ class SiameseCNN(torch.nn.Module):
 
 
     def concatenate(self, path1, path2):
-        return torch.cat((path1, path2))
+        return path1 + path2
 
 
     def flatten(self, x):
-        return torch.flatten(x, start_dim = 0, end_dim = -1)
+        return torch.flatten(x, start_dim = 1)
 
 
-    def propagate_path(self, x):
-        x = x
-        for layer in self.layers:
-            x = self.layers[layer](x)
+    def propagate_path(self, x): # TODO apply this as a recursive function
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+        x = self.layer8(x)
+        x = self.layer9(x)
+        x = self.layer10(x)
+        x = self.layer11(x)
+        x = self.layer12(x)
+        x = self.layer13(x)
+        x = self.layer14(x)
 
         return x
 
@@ -108,9 +119,9 @@ class SiameseCNN(torch.nn.Module):
         path1 = self.flatten(path1)
         path2 = self.flatten(path2)
         concatenated = self.concatenate(path1, path2)
-        concatenated = self.fully_connected_layer(concatenated.size(0), 4096)
-        concatenated = self.fully_connected_layer(4096, 4096)
-        return self.fully_connected_layer(4096, self.output_dims)
+        concatenated = self.fully_connected_layer(concatenated.size(1), 4096, concatenated)
+        concatenated = self.fully_connected_layer(4096, 4096, concatenated)
+        return self.fully_connected_layer(4096, self.output_dims, concatenated)
 
 
 
