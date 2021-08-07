f69370684949cf42a001258b2098cab8feecb7b2
unknown <s.dg.emir@gmail.com>
2021-08-07 11:59:53 +0200

    updated resnet hyperparameters

--- architectures/network_hyperparameters.py
+++ architectures/network_hyperparameters.py
@@ -2,9 +2,13 @@ import torch
 
 resnet = {
     'OPTIMIZER' : torch.optim.Adam, 
-    'LEARNING_RATE' : 0.0001,
+    'LEARNING_RATE' : 0.00002/3,
     'CRITERION' : torch.nn.CrossEntropyLoss, 
-    'EPOCHS' : 1000, 
+    'EPOCHS' : 60, 
+    'BATCH_SIZE' : 64,
+    'WEIGHT_DECAY' :0.009*4,
+    'DROPOUT_PROB' : 0.5,
+    'DROPOUT_LOC' : None,
     'SCHEDULER' : False,
     'DEVICE' : 'cuda' if torch.cuda.is_available() else 'cpu'
     }
