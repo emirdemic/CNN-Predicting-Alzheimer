9f730db369884481345f60f4a04d88437bdee19f
unknown <s.dg.emir@gmail.com>
2021-08-07 12:05:26 +0200

    added densenet

--- architectures/pretrained_architectures.py
+++ architectures/pretrained_architectures.py
@@ -1,5 +1,6 @@
 import torchvision 
 import torch
+from architectures import network_hyperparameters
 
 
 def get_resnet(output, resnet_version, pretrained = True, freeze = True):
@@ -18,10 +19,17 @@ def get_resnet(output, resnet_version, pretrained = True, freeze = True):
 
     if freeze == True:
         for param in model.parameters():
-            param.require_grads = False
+            param.requires_grad = False
 
     in_features = model.fc.in_features
-    model.fc = torch.nn.Linear(in_features, output)
+    # model.fc = torch.nn.Linear(in_features, output)
+
+    model.fc = torch.nn.Sequential(
+        torch.nn.Dropout(network_hyperparameters.resnet['DROPOUT_PROB']),
+        torch.nn.Linear(model.fc.in_features, output)
+    )
+    network_hyperparameters.resnet['DROPOUT_LOC'] = "Before last layer"
+    
 
     return model
 
@@ -30,7 +38,7 @@ def get_vgg19(output, pretrained = True, freeze = True):
     model = torchvision.models.vgg19(pretrained = pretrained)
     if freeze == True:
         for param in model.parameters():
-            param.require_grads = False 
+            param.requires_grad = False 
     
     model.classifier[-1] = torch.nn.Linear(model.classifier[-1].in_features, output)
     
@@ -41,8 +49,20 @@ def get_inception3(output, pretrained = True, freeze = True):
     model = torchvision.models.inception_v3(pretrained = True)
     if freeze == True:
         for param in model.parameters():
-            param.require_grads = False 
+            param.requires_grad = False 
     
     model.fc = torch.nn.Linear(model.fc.in_features, output)
 
+    return model
+
+def get_densenet(output, densenet_version = 121, pretrained = True, freeze = True):
+    model = torchvision.models.densenet121(pretrained = pretrained)
+    if freeze == True:
+        for param in model.parameters():
+            param.requires_grad = False 
+    print(model)
+    model.classifier = torch.nn.Sequential(
+        torch.nn.Dropout(0.5),
+        torch.nn.Linear(model.classifier.in_features, output)
+    )
     return model
